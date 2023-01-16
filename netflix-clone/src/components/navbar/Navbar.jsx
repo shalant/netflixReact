56a10c15183083d96d2511007aac6fb256ce2ac1
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-01-16 11:29:59 -0600

    scrolling navbar effect

--- netflix-clone/src/components/navbar/Navbar.jsx
+++ netflix-clone/src/components/navbar/Navbar.jsx
@@ -1,9 +1,19 @@
-import {Search} from '@mui/icons-material/';
-import { AcUnit } from "@mui/icons-material"
+import {Search, Notifications, ArrowDropDown} from '@mui/icons-material/';
+import { AcUnit } from "@mui/icons-material";
 
 import "./Navbar.scss";
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => window.onscroll = null;
+    };
+
+    console.log(isScrolled)
+
   return (
     <div className="navbar">
       <div className="container">
@@ -19,7 +29,17 @@ const Navbar = () => {
           <span>My List</span>
         </div>
         <div className="right">
-            <Search/>
+            <Search className='icon'/>
+            <span>KID</span>
+            <Notifications className='icon'/>
+            <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto+compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+            <div className="profile">
+                <ArrowDropDown className='icon'/>
+                <div className="options">
+                    <span>Settings</span>
+                    <span>Logout</span>
+                </div>
+            </div>
         </div>
       </div>
     </div>
