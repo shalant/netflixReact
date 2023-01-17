10ef08fe6f4118cac58915ec50ac82331511ebce
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-01-16 18:16:33 -0600

    54:45

--- netflix-clone/src/components/list/List.jsx
+++ netflix-clone/src/components/list/List.jsx
@@ -1,9 +1,35 @@
+import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from "@mui/icons-material";
 import "./List.scss";
+import ListItem from "../listItem/ListItem";
 
 export default function List() {
   return (
     <div className="list">
-        List
+        <span className="listTitle">Continue to watch</span>
+        <div className="wrapper">
+            <ArrowBackIosNewOutlined className="sliderArrow"/>
+            <div className="container">
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+            </div>
+            <ArrowForwardIosOutlined className="sliderArrow right"/>
+        </div>
     </div>
   )
 }
