f98ee06ddf6a38453838c45815d112b654ae29b0
Douglas Rosenberg <doug.rosenberg@gmail.com>
2023-01-17 10:03:11 -0600

    register page, working on css hacks

--- netflix-clone/src/pages/register/Register.jsx
+++ netflix-clone/src/pages/register/Register.jsx
@@ -4,8 +4,10 @@ export default function Register() {
   return (
     <div className="register">
         <div className="top">
-            <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" />
-            <button className="loginButton">Sign In</button>
+            <div className="wrapper">
+                <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" />
+                <button className="loginButton">Sign In</button>
+            </div>
         </div>
         <div className="container">
             <h1>Unlimited movies, TV shows, and more</h1>
@@ -13,6 +15,10 @@ export default function Register() {
             <p>
                 Ready to watch? Enter your email to create or restart your membership
             </p>
+            <div className="input">
+                <input type="email" placeholder="email address" />
+                <button className="registerButton">Get started</button>
+            </div>
         </div>
     </div>
   )
